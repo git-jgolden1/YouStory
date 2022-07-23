2c3aab060717669a9a9760114babfc98f166ad40
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2022-07-22 19:08:41 -0700

    Created 6 buttons with colors

--- YouStory/ContentView.swift
+++ YouStory/ContentView.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State private var numberOfButtons = 6
+	let buttonColorScheme: [Color] = [.red, .blue, .green, .yellow, .orange, .purple]
+	
+	
 	var body: some View {
-		Text("There is a world of possibilities awaiting you!")
-			.multilineTextAlignment(.center)
+		Text("Example message")
+			.font(.largeTitle)
+		Spacer()
+		LazyVGrid(columns: [GridItem(), GridItem()]) {
+			ForEach(0..<numberOfButtons) { i in
+				ZStack {
+					Circle()
+						.frame(width: 125, height: 125)
+					Button("B\(i + 1)") {
+						print("Button \(i + 1)")
+					}
+				}
+				.foregroundColor(buttonColorScheme[i])
+			}
+		}
+		Spacer()
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
